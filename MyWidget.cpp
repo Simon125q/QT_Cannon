e50697622dd1a7ff439f69f182743e5beef9a5a3
Simon125q <106029626+Simon125q@users.noreply.github.com>
2023-12-21 09:27:52 +0100

    testing layouts, slider and lcd number

--- MyWidget.cpp
+++ MyWidget.cpp
@@ -1,21 +1,40 @@
 #include <QApplication>
 #include <QFont>
 #include <QPushButton>
+#include <QLCDNumber>
+#include <QSlider>
 #include <QWidget>
+#include <QVBoxLayout>
 #include "MyWidget.h"
 
 MyWidget::MyWidget(QWidget *parent)
     :QWidget(parent)
 {
-    setFixedSize(700, 340);
+    //setFixedSize(700, 340);
 
     // create child widget
     QPushButton *quit = new QPushButton(tr("Quit"), this);
-    quit->setGeometry(50, 50, 300, 250);
+    quit->setGeometry(50, 50, 700, 250);
     quit->setFont(QFont("Times", 18, QFont::Bold));
+
+    QLCDNumber *lcd = new QLCDNumber(3);
+    lcd -> setSegmentStyle(QLCDNumber::Filled);
+
+    QSlider *slider = new QSlider(Qt::Horizontal);
+    slider -> setRange(0, 999);
+    slider -> setValue(0);
+
 
     //qApp pointer is a global variable declared in the 
     //<QApplication> header file. It points to the application's 
     //unique QApplication instance.
     connect(quit, SIGNAL(clicked()), qApp, SLOT(quit()));
+    connect(slider, SIGNAL(valueChanged(int)), lcd, SLOT(display(int)));
+
+    //manage the geometry of its child widgets
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(quit);
+    layout->addWidget(lcd);
+    layout->addWidget(slider);
+    setLayout(layout);
 }
